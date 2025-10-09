4ce66f9c2c3af2f4fd55cf1a69d44a709817db37
Theodore <tlcfem@gmail.com>
2025-10-09 03:00:23 +0000

    Refactor VirusTotal integration with VTClient class (#26)

--- dist/index.js
+++ dist/index.js
@@ -37014,57 +37014,163 @@ function wrappy (fn, cb) {
 /***/ 9431:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
-const fs = __nccwpck_require__(3024)
-
+const core = __nccwpck_require__(7484)
+const github = __nccwpck_require__(3228)
 const axios = __nccwpck_require__(7269)
 const FormData = __nccwpck_require__(6454)
+const { RateLimiter } = __nccwpck_require__(5157)
+const fs = __nccwpck_require__(3024)
+const path = __nccwpck_require__(6760)
 
-/**
- * https://docs.virustotal.com/reference/files-scan
- * @function vtUpload
- * @param {String} filePath
- * @param {String} apiKey
- * @return {Promise<*|Object>}
- */
-async function vtUpload(filePath, apiKey) {
-    console.log('vtUpload:', filePath)
-    const form = new FormData()
-    form.append('file', fs.createReadStream(filePath))
-    const url = await vtGetURL(filePath, apiKey)
-    console.log('url:', url)
-    const response = await axios.post(url, form, {
-        headers: { 'x-apikey': apiKey, ...form.getHeaders() },
-    })
-    // console.log('response.data:', response.data)
-    return response.data
-}
-
-/**
- * https://docs.virustotal.com/reference/files-upload-url
- * @function vtGetURL
- * @param {String} filePath
- * @param {String} apiKey
- * @return {Promise<*|String>}
- */
-async function vtGetURL(filePath, apiKey) {
-    // This does not consume per-minute api quota, consider using axios
-    const stats = fs.statSync(filePath)
-    console.log('stats.size:', stats.size)
-    if (stats.size < 32000000) {
-        return 'https://www.virustotal.com/api/v3/files'
+class VTClient {
+    /**
+     * @param {String} apiKey
+     */
+    #apiKey
+    #limiter = null
+    constructor(apiKey, rate) {
+        this.#apiKey = apiKey
+        if (rate) {
+            this.#limiter = new RateLimiter({
+                tokensPerInterval: rate,
+                interval: 'minute',
+            })
+        }
     }
 
-    const response = await axios.get(
-        'https://www.virustotal.com/api/v3/files/upload_url',
-        {
-            headers: { accept: 'application/json', 'x-apikey': apiKey },
+    /**
+     * Get the correct upload URL for the file size
+     * @param {String} filePath
+     * @return {Promise<String>}
+     */
+    async #getUploadURL(filePath) {
+        const stats = fs.statSync(filePath)
+        console.log('stats.size:', stats.size)
+        if (stats.size < 32000000) return 'https://www.virustotal.com/api/v3/files'
+        const response = await axios.get(
+            'https://www.virustotal.com/api/v3/files/upload_url',
+            {
+                headers: { accept: 'application/json', 'x-apikey': this.#apiKey },
+            }
+        )
+        return response.data.data
+    }
+
+    /**
+     * Upload a file to VirusTotal
+     * @param {String} filePath
+     * @return {Promise<Object>}
+     */
+    async #upload(filePath) {
+        console.log('vtUpload:', filePath)
+        const form = new FormData()
+        form.append('file', fs.createReadStream(filePath))
+        const url = await this.#getUploadURL(filePath)
+        console.log('url:', url)
+        const response = await axios.post(url, form, {
+            headers: { 'x-apikey': this.#apiKey, ...form.getHeaders() },
+        })
+        return response.data
+    }
+
+    /**
+     * Process a file
+     * @param {String} name
+     * @param {String} filePath
+     * @return {Promise<{name, link: string, id}>}
+     */
+    async #process(name, filePath) {
+        if (this.#limiter) {
+            const remainingRequests = await this.#limiter.removeTokens(1)
+            console.log('remainingRequests:', remainingRequests)
         }
-    )
-    // console.log('response.data:', response.data)
-    return response.data.data
+        const response = await this.#upload(filePath)
+        console.log('response.data.id:', response.data.id)
+        const link = `https://www.virustotal.com/gui/file-analysis/${response.data.id}`
+        console.log('link:', link)
+        return { id: response.data.id, name, link }
+    }
+
+    /**
+     * Process Files
+     * @param {String[]} files
+     * @return {Promise<Object[{id, name, link}]>}
+     */
+    async processFiles(files) {
+        if (!files.length) {
+            throw new Error('No files to process.')
+        }
+        core.startGroup('Processing Files')
+        console.log(files)
+        core.endGroup() // Files
+        const results = []
+        for (const file of files) {
+            const name = path.basename(file)
+            core.startGroup(`Processing: \u001b[36m${name}`)
+            results.push(await this.#process(name, file))
+            core.endGroup() // Processing
+        }
+        return results
+    }
+
+    /**
+     * Process Release Assets
+     * @param {InstanceType<typeof github.GitHub>} octokit
+     * @param {String} release_id
+     * @return {Promise<Object[{id, name, link}]>}
+     */
+    async processRelease(octokit, release_id) {
+        core.startGroup('Processing Release Assets')
+
+        // Get Assets
+        let page = 0
+        const allAssets = []
+        const { data } = await octokit.rest.rateLimit.get()
+        const ghLimiter = new RateLimiter({
+            tokensPerInterval: data.resources.core.limit,
+            interval: 'hour',
+        })
+        while (true) {
+            await ghLimiter.removeTokens(1)
+            const assets = await octokit.rest.repos.listReleaseAssets({
+                ...github.context.repo,
+                release_id: release_id,
+                per_page: 100,
+                page: ++page,
+            })
+            if (!assets.data.length) break
+            allAssets.push(...assets.data)
+        }
+        if (!allAssets.length) {
+            throw new Error(`No Assets Found for Release: ${release_id}`)
+        }
+
+        // Create Temp
+        console.log('RUNNER_TEMP:', process.env.RUNNER_TEMP)
+        const assetsPath = path.join(process.env.RUNNER_TEMP, 'assets')
+        console.log('assetsPath:', assetsPath)
+        if (!fs.existsSync(assetsPath)) fs.mkdirSync(assetsPath)
+
+        // Process Assets
+        const files = []
+        for (const asset of allAssets) {
+            const filePath = path.join(assetsPath, asset.name)
+            const file = await octokit.rest.repos.getReleaseAsset({
+                ...github.context.repo,
+                asset_id: asset.id,
+                headers: { Accept: 'application/octet-stream' },
+            })
+            fs.writeFileSync(filePath, Buffer.from(file.data))
+            files.push(filePath)
+        }
+
+        core.endGroup() // Assets
+
+        return await this.processFiles(files)
+    }
 }
 
-module.exports = vtUpload
+module.exports = VTClient
 
 
 /***/ }),
@@ -44350,16 +44456,11 @@ module.exports = /*#__PURE__*/JSON.parse('{"application/1d-interleaved-parityfec
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-const fs = __nccwpck_require__(3024)
-const path = __nccwpck_require__(6760)
-
 const core = __nccwpck_require__(7484)
 const github = __nccwpck_require__(3228)
 const glob = __nccwpck_require__(7206)
 
-const { RateLimiter } = __nccwpck_require__(5157)
-
-const vtUpload = __nccwpck_require__(9431)
+const VTClient = __nccwpck_require__(9431)
 
 ;(async () => {
     try {
@@ -44369,25 +44470,26 @@ const vtUpload = __nccwpck_require__(9431)
         core.startGroup('Inputs')
         const inputs = getInputs()
         console.log(inputs)
-        core.endGroup() // Inputs
 
         // Set Variables
         const octokit = github.getOctokit(inputs.token)
         const release = await getRelease(octokit, inputs.release_id)
-        const limiter = new RateLimiter({
-            tokensPerInterval: inputs.rate,
-            interval: 'minute',
-        })
+        const client = new VTClient(inputs.key, inputs.rate)
+
+        core.endGroup() // Inputs
 
         // Process
         /** @type {Object[]} */
         let results
         if (inputs.files?.length) {
             // core.info('📁 Processing File Globs')
-            results = await processFiles(inputs, limiter)
+            const globber = await glob.create(inputs.files.join('\n'), {
+                matchDirectories: false,
+            })
+            results = await client.processFiles(await globber.glob())
         } else if (release) {
             // core.info('🗄️ Processing Release Assets')
-            results = await processRelease(inputs, limiter, octokit, release)
+            results = await client.processRelease(octokit, release.id)
         } else {
             return core.setFailed('No files or release to process.')
         }
@@ -44487,7 +44589,6 @@ const vtUpload = __nccwpck_require__(9431)
 
         core.info('✅ \u001b[32;1mFinished Success')
     } catch (e) {
-        core.endGroup()
         console.log(e)
         if (e.response) {
             console.log(`\u001b[31m Error: ${e.response.statusText}`)
@@ -44496,128 +44597,6 @@ const vtUpload = __nccwpck_require__(9431)
         core.setFailed(e.message)
     }
 })()
-
-/**
- * Process Release Assets
- * @param {Object} inputs
- * @param {RateLimiter} limiter
- * @param {InstanceType<typeof github.GitHub>} octokit
- * @param {Object} release
- * @return {Promise<Object[{id, name, link}]>}
- */
-async function processRelease(inputs, limiter, octokit, release) {
-    core.startGroup('Processing Release Assets')
-    core.startGroup('Release')
-    console.log(release)
-    core.endGroup() // Release
-
-    // Get Assets
-    let allAssets = []
-    let page = 0
-    const { data } = await octokit.rest.rateLimit.get()
-    const ghLimiter = new RateLimiter({
-        tokensPerInterval: data.resources.core.limit,
-        interval: 'hour',
-    })
-    while (true) {
-        await ghLimiter.removeTokens(1)
-        const assets = await octokit.rest.repos.listReleaseAssets({
-            ...github.context.repo,
-            release_id: release.id,
-            per_page: 100,
-            page: ++page,
-        })
-        if (!assets.data.length) break
-        allAssets = allAssets.concat(assets.data)
-    }
-    if (!allAssets.length) {
-        throw new Error(`No Assets Found for Release: ${release.id}`)
-    }
-
-    // Create Temp
-    console.log('RUNNER_TEMP:', process.env.RUNNER_TEMP)
-    const assetsPath = path.join(process.env.RUNNER_TEMP, 'assets')
-    console.log('assetsPath:', assetsPath)
-    if (!fs.existsSync(assetsPath)) {
-        fs.mkdirSync(assetsPath)
-    }
-
-    console.log(allAssets)
-    core.endGroup() // Assets
-
-    // Process Assets
-    const results = []
-    for (const asset of allAssets) {
-        core.startGroup(`Processing: \u001b[36m${asset.name}`)
-        if (inputs.rate) {
-            const remainingRequests = await limiter.removeTokens(1)
-            console.log('remainingRequests:', remainingRequests)
-        }
-        const filePath = path.join(assetsPath, asset.name)
-        console.log('filePath:', filePath)
-        const file = await octokit.rest.repos.getReleaseAsset({
-            ...github.context.repo,
-            asset_id: asset.id,
-            headers: {
-                Accept: 'application/octet-stream',
-            },
-        })
-        fs.writeFileSync(filePath, Buffer.from(file.data))
-        const result = await processVt(inputs, asset.name, filePath)
-        console.log('result:', result)
-        results.push(result)
-        core.endGroup() // Processing
-    }
-    return results
-}
-
-/**
- * Process File Globs
- * @param {Object} inputs
- * @param {RateLimiter} limiter
- * @return {Promise<Object[{id, name, link}]>}
- */
-async function processFiles(inputs, limiter) {
-    const globber = await glob.create(inputs.files.join('\n'), {
-        matchDirectories: false,
-    })
-    const files = await globber.glob()
-    core.startGroup('Processing File Globs')
-    console.log(files)
-    core.endGroup() // Files
-    if (!files.length) {
-        throw new Error('No files to process.')
-    }
-    const results = []
-    for (const file of files) {
-        const name = file.split('\\').pop().split('/').pop()
-        core.startGroup(`Processing: \u001b[36m${name}`)
-        if (inputs.rate) {
-            const remainingRequests = await limiter.removeTokens(1)
-            console.log('remainingRequests:', remainingRequests)
-        }
-        const result = await processVt(inputs, name, file)
-        // console.log('result:', result)
-        results.push(result)
-        core.endGroup() // Processing
-    }
-    return results
-}
-
-/**
- * Process VirusTotal
- * @param {Object} inputs
- * @param {String} name
- * @param {String} filePath
- * @return {Promise<{name, link: string, id}>}
- */
-async function processVt(inputs, name, filePath) {
-    const response = await vtUpload(filePath, inputs.key)
-    console.log('response.data.id:', response.data.id)
-    const link = `https://www.virustotal.com/gui/file-analysis/${response.data.id}`
-    console.log('link:', link)
-    return { id: response.data.id, name, link }
-}
 
 /**
  * Get Release
